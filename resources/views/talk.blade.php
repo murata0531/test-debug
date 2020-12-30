00226bf0ee1298e7cc2ddcd6a2f4267607a001fc
murata0531 <coless115@gmail.com>
2020-12-30 22:08:45 +0900

    ok

--- resources/views/talk.blade.php
+++ resources/views/talk.blade.php
@@ -376,15 +376,7 @@
     </div>
     <div id="talk"></div>
     <script src="{{asset('/js/app.js')}}"></script>
-    <div>
-
-        <!-- <script>
-        $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    }); -->
-        </script>
+   
 </body>
 
 </html>
