f7d1e91a7d8f4e687d8654b01e7ed744b696ea18
murata0531 <coless115@gmail.com>
2020-12-31 14:21:08 +0900

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -15,26 +16,23 @@
                 margin: 0;
                 padding: 0;
             }
+
             body{
                 top: 0px;
                 left: 0px;
                 width: 1920px;
                 min-height: 100vh;    
-                background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-                background: #FFFFFF 0% 0% no-repeat padding-box;
+                background: #FFFFFF;
                 opacity: 1;
             }
-            ​
-            ​
+
             .left_all{
                 min-height: 100vh;
                 width: 18vw;
-                background: var(--unnamed-color-62abb6) 0% 0% no-repeat padding-box;
-                background: #62ABB6 0% 0% no-repeat padding-box;
+                background: #62ABB6;
                 opacity: 1;
             }
-            ​
-            ​
+​
             .test{
                 display: flex;
                 -webkit-box-align: start;
@@ -50,7 +48,7 @@
                 overflow-x: hidden;
                 overflow-y: auto;
             }
-            ​
+
             .nav-list{
                 -webkit-box-flex: 0;
                 -ms-flex: 0 0 auto;
@@ -58,32 +56,80 @@
                 width: 100%;
                 list-style-type: none;
             }
-            ​
-            .nav-list li{
+​
+            .nav-list > li{
                 vertical-align: middle; 
             } 
-            ​
-            /*  *, ::after, ::before {
-                -webkit-box-sizing: border-box;
-                box-sizing: border-box; 
-            } */
-            ​
+​
+
+​
             .left1{
                 position: relative;
                 height: 8vh;
                 padding-left: 3vw;
                 position: relative;
-                color: var(--unnamed-color-ffffff);
-                font: normal normal normal 24px/38px Noto Sans JP;
+                color: white;
                 letter-spacing: 2.4px;
-                color: #FFFFFF;
                 opacity: 1;
             }
-
         </style>
     </head>
 
     <body>
         
+        <!-- 左側のもの -->
+        <div class="left_all">
+            <div class="test">
+                <br><br>
+                <ul class="nav-list">
+                    <li>
+                        <div class=" left1" id="aaa">
+                            <i class="fas fa-home">
+                            </i>
+                            ホーム
+                        </div>
+                    </li>
+                    <li>
+                        <div class="left1" id="aa">
+                            <i class="fas fa-clipboard-list">
+                            </i>
+                            タスク
+                        </div>
+                    </li>
+                    <li>
+                        <div class="left1">
+                            <i class="far fa-comment-alt">
+                            </i>
+                            トーク
+                        </div>
+                    </li>
+                    <li>
+                        <div class="left1">
+                            <i class="fas fa-book-open">
+                            </i>
+                            ノート
+                        </div>
+                    </li>
+                    <li>
+                        <div class="left1">
+                            <i class="far fa-folder">
+                            </i>
+                            ファイル共有
+                        </div>
+                    </li>
+                    <li>
+                        <div class="left1">
+                            <i class="far fa-file-alt">
+                                
+                            </i>
+                            レポート
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- 真ん中のもの -->
+        <div class="center_all">
+        </div>
     </body>
 </html>
